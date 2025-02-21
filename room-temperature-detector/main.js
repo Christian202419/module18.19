d33d51a35adc32ac97383fb1def3c3cb110cf92e
Christian202419 <164139392+Christian202419@users.noreply.github.com>
2025-02-21 11:23:42 +0800

    Update main.js

--- room-temperature-detector/main.js
+++ room-temperature-detector/main.js
@@ -1,31 +1,23 @@
 const http = require('http');
 const fs = require('fs');
 
-
 function generateRandomTemperature() {
     return Math.floor(Math.random() * (35 - 15 + 1) + 15);
 }
 
-
 const server = http.createServer((req, res) => {
-    // Read the HTML file
     fs.readFile('index.html', 'utf8', (err, data) => {
         if (err) {
             res.statusCode = 500;
             res.end('Error loading the HTML file');
         } else {
-
             const temperature = generateRandomTemperature();
-
             const modifiedData = data.replace('-', temperature);
-
             res.setHeader('Content-Type', 'text/html');
-
             res.end(modifiedData);
         }
     });
 });
-
 
 server.listen(3000, () => {
     console.log('Server running on port 3000');
